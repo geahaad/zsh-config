5f3ded36924636368946c43123f0363228c9f883
Gerhard Dinhof <geahaad@Geahaad-MBP.local>
2017-09-16 10:35:39 +0200

    initial setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# install zsh
+
+# install oh-my-zsh
+
+### install plugins
+
+# zsh-syntax-highlighting
+cd ~/.oh-my-zsh/custom/plugins
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
