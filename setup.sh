992ac80a00bcd6b1ff6c3f62bd952e98d6ba2e90
Gerhard Dinhof <gerhard.dinhof@at.ibm.com>
2017-09-22 07:54:08 +0200

    powerlevel9k added

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # install oh-my-zsh
 
 ### install plugins
+# powerlevel9k theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # zsh-syntax-highlighting
 cd ~/.oh-my-zsh/custom/plugins
