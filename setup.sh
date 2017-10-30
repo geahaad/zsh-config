5f41667f0c2981b4d36b7fa70a25de96667b1ed1
Gerhard Dinhof <gerhard.dinhof@at.ibm.com>
2017-10-30 07:35:11 +0100

    oh-my-zsh configuration and activation

--- setup.sh
+++ setup.sh
@@ -10,3 +10,7 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 # zsh-syntax-highlighting
 cd ~/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+
+# copy .zshrc and activate
+cp .zshrc ~
+source ~.zshrc
